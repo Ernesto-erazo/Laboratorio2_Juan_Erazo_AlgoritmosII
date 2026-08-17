6abb75627c1e21880bc8ce79cc211c81624922a6
ramir <ramir@ERNESTO>
2026-08-17 17:22:54 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio2_Juan_Erazo_AlgoritmosII/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Laboratorio2_Juan_Erazo_AlgoritmosII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Cliente: ");
+            string cliente = Console.ReadLine();
+
+            Console.Write("Precio: ");
+            decimal precio = decimal.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Cantidad:");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            decimal subtotal = precio * cantidad;
+            Console.WriteLine($"Subtotal: {subtotal:c}");
+        }
+    }
+}
