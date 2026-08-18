307df44895bde488a071fb89bb7852c6a48df95e
ramir <ramir@ERNESTO>
2026-08-18 07:56:23 -0600

    primer paso

---
+++ Guia1_Sesion2/Paso2_TexoNumeros/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Paso2_TexoNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto1 = "10";
+            string texto2 = "5";
+            string suma_de_textos = texto1 + texto2;
+            Console.WriteLine("suma de textos:" + suma_de_textos);
+
+            int numero1 = 10;
+            int numero2 = 5;
+            int suma_de_numeros = numero1 + numero2;
+            Console.WriteLine("suma de enteros:" + suma_de_numeros);    
+
+            Console.WriteLine($"Multiplicacion de enteros: {numero1 * numero2}");
+            
+        }
+    }
+}
